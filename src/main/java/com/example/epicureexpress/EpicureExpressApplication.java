f31a44f9798f11cfc3782a9382d6f6672840c73e
Alexey <alexey.verb.04@gmail.com>
2024-04-28 08:22:52 +0300

    Creating project files. Создание файлов проекта.

---
+++ src/main/java/com/example/epicureexpress/EpicureExpressApplication.java
@@ -0,0 +1,13 @@
+package com.example.epicureexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicureExpressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicureExpressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/epicureexpress/EpicureExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.epicureexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicureExpressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
